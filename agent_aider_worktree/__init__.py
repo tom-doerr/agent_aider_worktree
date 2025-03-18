c63eb7dd21b686cdc44a390a0028e7ac7d20f519
Tom Dörr <tomdoerr96@gmail.com>
2025-03-18 15:23:18 +0100

    style: Use double quotes for string literals in __init__.py

--- agent_aider_worktree/__init__.py
+++ agent_aider_worktree/__init__.py
@@ -3,4 +3,4 @@
 from .cli import main, setup_arg_parser
 from .core import create_worktree, merge_and_push
 
-__all__ = ['main', 'setup_arg_parser', 'create_worktree', 'merge_and_push']
+__all__ = ["main", "setup_arg_parser", "create_worktree", "merge_and_push"]
